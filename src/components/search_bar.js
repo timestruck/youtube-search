38b326d069f5cd7db30d61c415b8ff1e976bb026
Alice Xia <Alice@Alices-MacBook-Pro-2.local>
2018-05-01 15:05:15 -0400

    youtube search with react redux

---
+++ src/components/search_bar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
+    render() {
+        return (
+            <div className="search-bar">
+                <input 
+                    value={this.state.term}
+                    onChange={event => this.onSearchChange(event.target.value)} />
+            </div>
+        );
+    }
+
+    onSearchChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
+}
+
+export default SearchBar;
